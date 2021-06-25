a83dc47efc192df3abef2443e37f1a7901f46f4e
Isaac <zuderg@gmail.com>
2021-06-25 15:23:07 -0700

    added script

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - generates a random number and makes a statement about the last digit
+ * Return: will always be 0
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+char m = 'a';
+char n = '\n';
+int i;
+
+for (i = 0; i < 26; i++)
+{
+if (m != 'q' && m != 'e')
+{
+putchar(m);
+}
+m++;
+}
+putchar(n);
+return (0);
+}
