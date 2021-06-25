8bcdef4c6154180ca4dcd52e60e6d0046d3afdc5
Isaac <zuderg@gmail.com>
2021-06-25 08:20:54 -0700

    added scripts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,5 +6,10 @@
 */
 int main(void)
 {
-        return (0);
+	printf("Size of an char:  %zu byte(s)\n", sizeof(char));
+	printf("Size of an int:  %zu byte(s)\n", sizeof(int));
+	printf("Size of an long int:  %zu byte(s)\n", sizeof(long));
+	printf("Size of an long long int:  %zu byte(s)\n", sizeof(long long));
+	printf("Size of an float:  %zu byte(s)\n", sizeof(float));
+	return (0);
 }
