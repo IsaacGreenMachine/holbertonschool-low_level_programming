68ddd858ba4075c22acbb4798562bd8cdfe9814d
Isaac <zuderg@gmail.com>
2021-06-25 15:26:43 -0700

    added script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - generates a random number and makes a statement about the last digit
+ * Return: will always be 0
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
