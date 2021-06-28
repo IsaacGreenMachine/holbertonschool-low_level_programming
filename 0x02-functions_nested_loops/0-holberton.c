41ab2839b443baa4073ac784d1d177ea2506bf4d
Isaac <zuderg@gmail.com>
2021-06-28 12:47:23 -0700

    added script

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+/* more headers goes there */
+
+/**
+ * main - generates a random number and makes a statement about the last digit
+ * Return: will always be 0
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+}
