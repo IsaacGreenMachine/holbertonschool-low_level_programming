40f3fc7d79ed89ee863e2004f97001e1a25b1919
Isaac <zuderg@gmail.com>
2021-06-28 14:08:12 -0700

    changed indentations

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,25 @@
 /* more headers goes there */
 
 /**
- * print_alphabet - generates a random number and makes a statement about the last digit
+ * print_alphabet - prints alphabet in lowercase using _putchar
  * Return: will always be void
  * betty style doc for function main goes there
- */
-
- void print_alphabet(void){
-int i;
-	 for (i=0; i < 26; i++)
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+void print_alphabet(void)
 {
-_putchar(i+97);
+int i;
+for (i = 0; i < 26; i++)
+{
+_putchar(i + 97);
 }
 	 _putchar('\n');
 }
