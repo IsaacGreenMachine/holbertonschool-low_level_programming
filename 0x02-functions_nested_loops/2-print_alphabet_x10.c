d38e9139f88b9150bf2412d89f08433a1daf5f47
Isaac <zuderg@gmail.com>
2021-06-28 14:14:07 -0700

    added scripts

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet - prints alphabet in lowercase using _putchar
+ * Return: will always be void
+ * betty style doc for function main goes there
+*/
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 26; i++)
+{
+_putchar(i + 97);
+}
+         _putchar('\n');
+}
+}
