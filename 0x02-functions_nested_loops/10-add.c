a8f4d192f2fa17671a115c6aa0bdca5fe406ffb2
Isaac <zuderg@gmail.com>
2021-06-28 17:27:43 -0700

    added scripts

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *add- prints sum of two inputs
+ *@n: First operand
+ *@m: Second operand
+ *
+ * Return: will always be abs(n)
+*/
+
+int add(int n, int m)
+{
+	return (n + m);
+}
