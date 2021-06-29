7e04009f45c5a37a1904f4dc9b6f1b1075dac35c
Isaac <zuderg@gmail.com>
2021-06-28 18:05:47 -0700

    fixed spacing issue

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,34 @@ void print_to_98(int n)
 {
 if (n == 98)
 {
-printf("%i, ", n);
+printf("%i", n);
 }
 else if (n < 98)
 {
 for (n = n; n < 99; n++)
 {
+if (n == 98)
+{
+printf("%i", n);
+}
+else
+{
 printf("%i, ", n);
+}
 }
 }
 else
 {
 for (n = n; n > 97; n--)
 {
+if (n == 98)
+{
+printf("%i", n);
+}
+else
+{
 printf("%i, ", n);
+}
 }
 }
 printf("\n");
