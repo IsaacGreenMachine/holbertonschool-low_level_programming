4fb857dfe002338874a5b7d144919225bcc195d0
Isaac <zuderg@gmail.com>
2021-06-28 17:18:36 -0700

    added script

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ *times_table- prints absolute value
+ * Return: will always be abs(n)
+*/
+
+void times_table(void)
+{
+int i = 1;
+int j = 1;
+int q;
+int r;
+int s;
+for (j = 0; j < 10; j++)
+{
+	for (i = 0; i < 10; i++)
+	{
+		q = (i * j);
+		r = (q / 10);
+		s = (q % 10);
+		if (i > 0)
+		{
+		_putchar(' ');
+		}
+		if (q < 10)
+		{
+		_putchar(' ');
+		}
+		if (q < 10)
+		{
+		_putchar(q + '0');
+		}
+		else
+		{
+		_putchar(r + '0');
+		_putchar(s + '0');
+		}
+		if (i < 9)
+		{
+		_putchar(',');
+		}
+	}
+	_putchar('\n');
+}
+}
