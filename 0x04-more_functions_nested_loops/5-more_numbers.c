2d0fe48ceb3329bd6dfa38db99bbd7cdd247cc88
Isaac <zuderg@gmail.com>
2021-07-01 15:44:00 -0700

    added scripts

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *more_numbers- prints numbers from 0-14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i;
+int j;
+for (j = 0; j < 11; j++)
+{
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+for (i = 0; i < 5; i++)
+{
+_putchar('1');
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+}
