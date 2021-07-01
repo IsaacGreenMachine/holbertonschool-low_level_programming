fb782998335b0221cf8a041d289272d2b64f43cf
Isaac <zuderg@gmail.com>
2021-07-01 15:00:41 -0700

    added script and header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *mul- multiplies two numbers
+ *@a: 1st number
+ *@b: 2nd number
+ *
+ * Return: product of two inputs.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
