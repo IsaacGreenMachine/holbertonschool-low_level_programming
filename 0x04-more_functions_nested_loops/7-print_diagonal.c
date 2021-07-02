5080a885430dd33a6468c18a5d2e4d3922a7f37b
Isaac <zuderg@gmail.com>
2021-07-01 19:26:32 -0700

    added script

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *print_diagonal- draws a diagonal line
+ *@n: length of the diagonal line
+ *
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n <= 0)
+{
+_putchar('\n');
+}
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
