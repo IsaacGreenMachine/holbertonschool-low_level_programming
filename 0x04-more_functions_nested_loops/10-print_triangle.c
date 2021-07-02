7fd12dd8f7a386f4a06b7266639b05fbc34b644e
Isaac <zuderg@gmail.com>
2021-07-01 21:04:35 -0700

    added the script

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *print_triangle- prints a right triangle of size size.
+ *@size: size of triangle
+ *
+ * Return: void.
+ */
+void print_triangle(int size)
+{
+int d;
+int i;
+int j;
+for (d = size - 1; d >= 0; d--)
+{
+for (i = 0; i < d; i++)
+{
+	_putchar(' ');
+}
+for (j = 0; j < size - d; j++)
+{
+	_putchar('#');
+}
+_putchar('\n');
+}
+if (size == 0)
+{
+_putchar('\n');
+}
+}
