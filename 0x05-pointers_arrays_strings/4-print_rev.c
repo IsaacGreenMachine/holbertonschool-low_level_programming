8ba87f4afba2bfdf3e3e5212c270c996c64e203c
Isaac <zuderg@gmail.com>
2021-07-06 12:52:34 -0700

    added script

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the start of a string of chars
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+char *start = s;
+while (*s != 0)
+{
+s++;
+}
+for (s = s; s >= start; s--)
+{
+_putchar(*s);
+}
+_putchar('\n');
+}
