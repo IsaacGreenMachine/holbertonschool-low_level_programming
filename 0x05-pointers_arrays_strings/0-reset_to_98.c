4ebfd2de05351ba33135c359552a22082faca9b5
Isaac <zuderg@gmail.com>
2021-07-06 10:36:58 -0700

    added script

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - sets the passed pointer's value to 98.
+ * @n: pointer whose pointed to item's value is set to 98.
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
