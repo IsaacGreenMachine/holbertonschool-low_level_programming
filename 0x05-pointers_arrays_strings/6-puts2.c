404dd81ca62844d8702a256a00002ce7b99493db
Isaac <zuderg@gmail.com>
2021-07-06 20:48:56 -0700

    added scripts and updated header

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other char of a string.
+ * @str: passed pointer to the start of a string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+while (*str != 0)
+{
+_putchar(*str);
+str++;
+str++;
+}
+_putchar('\n');
+}
