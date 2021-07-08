7dfc46fea8341a7104042543b4fce065d42ba57e
Isaac <zuderg@gmail.com>
2021-07-08 12:35:41 -0700

    added scripts and header file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: final destination of catted strings
+ * @src: string to be catted to dest
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+char *i = dest;
+char *j = src;
+while (*i != 0)
+{
+i++;
+}
+while (*j != 0)
+{
+*i = *j;
+i++;
+j++;
+}
+*i = 0;
+return (dest);
+}
