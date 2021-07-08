5f2b5937030fa30967a724b1f9442166020328c5
Isaac <zuderg@gmail.com>
2021-07-08 13:22:31 -0700

    added scripts and header file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+/**
+ * _strncpy - copies n characters from src to beginning of dest.
+ * @dest: pointer to the beginning of copy destination
+ * @src: pointer to beginning of what to copy
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to beginning of copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+char *i = dest;
+char *j = src;
+int count = 0;
+while (*j != 0 && count < n)
+{
+*i = *j;
+i++;
+j++;
+count++;
+}
+if (*j == 0)
+{
+*i = 0;
+}
+return (dest);
+}
