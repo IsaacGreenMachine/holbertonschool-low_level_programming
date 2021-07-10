cd762b2eeef933206e05e410926a5ac383620942
Isaac <zuderg@gmail.com>
2021-07-09 22:13:41 -0700

    added scripts and updated header file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: pointer to array to be searched
+ * @accept: list of chars to be searched for
+ *
+ * Return: pointer to first occurance
+ */
+char *_strpbrk(char *s, char *accept)
+{
+char *a;
+for( ; *s != 0; s++)
+{
+for (a = accept; *a != 0; a++)
+{
+if (*s == *a)
+{
+return (s);
+}
+}
+}
+return ('\0');
+}
