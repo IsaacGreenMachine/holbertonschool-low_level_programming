afa7d566bd154712430437be196d8f017568b8ca
Isaac <zuderg@gmail.com>
2021-07-09 21:18:20 -0700

    added scripts and updated header file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - searches passed string s for char c
+ * @s: start of string
+ * @c: char to be searched for
+ *
+ * Return: pointer to char location or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+for (; *s != 0; s++)
+{
+if (*s == c)
+{
+return (s);
+}
+}
+return ('\0');
+}
