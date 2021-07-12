491ee05e5eda1452fc417033dcbd23c334390893
Isaac <zuderg@gmail.com>
2021-07-12 11:25:04 -0700

    added skeleton scripts

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+/**
+ * _sqrt_recursion - calculate square root
+ * @n: number to be square rooted
+ *
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+return (n);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+/**
+ * is_prime_number - returns value based on primeness
+ * @n: number to be checked
+ *
+ * Return: Always 1 if prime 0 if not
+ */
+int is_prime_number(int n)
+{
+return (0);
+}
