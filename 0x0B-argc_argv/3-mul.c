90fc2062f86b9cfa18d7e048995c4ff8bf280944
Isaac <zuderg@gmail.com>
2021-07-19 11:49:36 -0700

    added script

--- 0x0B-argc_argv/3-mul.c
+++ 0x0B-argc_argv/3-mul.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: number of modifiers
@@ -5,6 +7,16 @@
  *
  * Return: void
  */
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+printf("%i\n", atoi(*(argv + 1)) * atoi(*(argv + 2)));
+}
+return (0);
 }
