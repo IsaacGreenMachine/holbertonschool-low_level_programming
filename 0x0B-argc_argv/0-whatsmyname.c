853ffd84419244b6f14a03530872912761414cce
Isaac <zuderg@gmail.com>
2021-07-19 08:42:08 -0700

    added return value

--- 0x0B-argc_argv/0-whatsmyname.c
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -6,13 +6,13 @@
  *
  * Return: void
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
-argc = 0;
 for (i = 0; *(argv[0] + i) != 0; i++)
 {
 _putchar(*(argv[0] + i));
 }
 _putchar('\n');
+return (0);
 }
