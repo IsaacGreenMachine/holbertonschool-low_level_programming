2a614aecb1c18e6bbf54d352742dcfa70f9fdcc0
Isaac <zuderg@gmail.com>
2021-07-19 12:29:51 -0700

    added scripts

--- 0x0B-argc_argv/4-add.c
+++ 0x0B-argc_argv/4-add.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - adds positive numbers
  * @argc: number of modifiers
@@ -5,6 +7,34 @@
  *
  * Return: void
  */
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
+int i;
+int sum = 0;
+	if (argc == 1)
+		{
+		sum = 0;
+		}
+	else
+		{
+		for (i = 1; i < argc; i++)
+		{
+		if (atoi(argv[i]) < 0)
+			{
+			printf("Error\n");
+			return (1);
+			}
+		else if (atoi(argv[i]) == 0 && argv[i] != 0)
+			{
+			printf("Error\n");
+			return (1);
+			}
+		else
+			{
+			sum = sum + atoi(argv[i]);
+			}
+		}
+		}
+	printf("%i\n", sum);
+	return (0);
 }
