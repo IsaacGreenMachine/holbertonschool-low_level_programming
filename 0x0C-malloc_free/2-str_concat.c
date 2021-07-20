8ab19b73da1fb6a56c9cf576d64962942075be4c
Isaac <zuderg@gmail.com>
2021-07-20 11:16:04 -0700

    added malloc check

--- 0x0C-malloc_free/2-str_concat.c
+++ 0x0C-malloc_free/2-str_concat.c
@@ -12,26 +12,42 @@ char *str_concat(char *s1, char *s2)
 if (s1 != NULL && s2 != NULL)
 {
 char *p = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+if (p == NULL)
+{
+return (NULL);
+}
 p = strcpy(p, s1);
 strcpy(p + strlen(s1), s2);
-return(p);
+return (p);
 }
 else if (s1 == NULL && s2 == NULL)
 {
 char *p = (char *)malloc(1);
+if (p == NULL)
+{
+return (NULL);
+}
 *p = 0;
-return(p);
+return (p);
 }
 else if (s1 == NULL)
 {
 char *p = (char *)malloc(strlen(s2) + 1);
+if (p == NULL)
+{
+return (NULL);
+}
 p = strcpy(p, s2);
-return(p);
+return (p);
 }
 else
 {
 char *p = (char *)malloc(strlen(s1) + 1);
 p = strcpy(p, s1);
-return(p);
+if (p == NULL)
+{
+return (NULL);
+}
+return (p);
 }
 }
