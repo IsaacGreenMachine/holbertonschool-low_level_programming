ee09acd4debc811b7a092e2127e97ade7ec68880
Isaac <zuderg@gmail.com>
2021-07-20 09:35:09 -0700

    added script, header file, and README

---
+++ 0x0C-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+if (size == 0)
+{
+return (NULL);
+}
+else
+{
+char* p = (char*)calloc(size, sizeof(char));
+for (i = 0; i < size; i++)
+{
+p[i] = c;
+}
+return(p);
+}
+}
---
+++ 0x0C-malloc_free/README.md
@@ -0,0 +1,1 @@
+this is a directory containing scripts that use malloc and free
