38ab7d8f62e974d0b3e5418d0236e93847c744b0
Isaac <zuderg@gmail.com>
2021-07-20 09:42:00 -0700

    added documentation

--- 0x0C-malloc_free/0-create_array.c
+++ 0x0C-malloc_free/0-create_array.c
@@ -1,4 +1,12 @@
 #include <stdlib.h>
+/**
+ * create_array - creates an array of size size initialized to c.
+ * @size: size of array
+ * @c: char to be initialized to array
+ *
+ *
+ * Return: pointer to array or NULL if failed.
+ */
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
