3eb7bf7fc1fc1e4b348dce63cee77f5eec7f2ba0
Isaac <zuderg@gmail.com>
2021-07-20 11:10:42 -0700

    shortened script

--- 0x0C-malloc_free/2-str_concat.c
+++ 0x0C-malloc_free/2-str_concat.c
@@ -5,34 +5,33 @@
  * @s1: first string
  * @s2: second string
  *
- *
  * Return: pointer to catted string or NULL if failed
  */
 char *str_concat(char *s1, char *s2)
 {
-char *p = (char *)malloc(strlen(s1) + strlen(s2));
-char *q = p;
-if (p == NULL)
+if (s1 != NULL && s2 != NULL)
 {
-return (NULL);
+char *p = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+p = strcpy(p, s1);
+strcpy(p + strlen(s1), s2);
+return(p);
 }
-if (s1 != NULL)
+else if (s1 == NULL && s2 == NULL)
 {
-while (*s1 != 0)
+char *p = (char *)malloc(1);
+*p = 0;
+return(p);
+}
+else if (s1 == NULL)
 {
-*p = *s1;
-p++;
-s1++;
+char *p = (char *)malloc(strlen(s2) + 1);
+p = strcpy(p, s2);
+return(p);
 }
-}
-if (s2 != NULL)
+else
 {
-while (*s2 != 0)
-{
-*p = *s2;
-p++;
-s2++;
+char *p = (char *)malloc(strlen(s1) + 1);
+p = strcpy(p, s1);
+return(p);
 }
-}
-return (q);
 }
