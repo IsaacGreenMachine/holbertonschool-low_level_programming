1b8bd4a6f1d7deee99cd848229ed156f18d0f268
Isaac <zuderg@gmail.com>
2021-07-21 12:07:50 -0700

    added script

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#define name __FILE__
+#include <stdio.h>
+/**
+ * main - prints file's name followed by newline.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+printf("%s\n", name);
+return (0);
+}
