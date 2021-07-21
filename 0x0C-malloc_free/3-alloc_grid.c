74d2b5672b7f8831f48fe38db456d8832bae7d08
Isaac <zuderg@gmail.com>
2021-07-21 10:44:22 -0700

    switched width and height

--- 0x0C-malloc_free/3-alloc_grid.c
+++ 0x0C-malloc_free/3-alloc_grid.c
@@ -13,15 +13,15 @@ if (width == 0 || height == 0)
 {
 return (NULL);
 }
-p = (int **)malloc(sizeof(int *) * width);
+p = (int **)malloc(sizeof(int *) * height);
 if (p == NULL)
 {
 free(p);
 return (NULL);
 }
-for (i = 0; i < width; i++)
+for (i = 0; i < height; i++)
 {
-p[i] = (int *)malloc(sizeof(int) * height);
+p[i] = (int *)malloc(sizeof(int) * width);
 if (p[i] == NULL)
 {
 for (; i >= 0; i--)
@@ -31,7 +31,7 @@ free(p[i]);
 free(p);
 return (NULL);
 }
-for (j = 0; j < height; j++)
+for (j = 0; j < width; j++)
 {
 *(p[i] + j) = 0;
 }
