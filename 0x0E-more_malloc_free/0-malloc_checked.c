35adba3b60cbe2296b51c8b4c3b13108e3defc23
Isaac <zuderg@gmail.com>
2021-07-23 12:02:57 -0700

    appeased betty

--- 0x0E-more_malloc_free/0-malloc_checked.c
+++ 0x0E-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,10 @@
 #include <stdlib.h>
+/**
+ * malloc_checked - returns a pointer to allocated space of size b
+ * @b: size of new space
+ *
+ * Return: pointer to new space
+ */
 void *malloc_checked(unsigned int b)
 {
 void *p = malloc(b);
