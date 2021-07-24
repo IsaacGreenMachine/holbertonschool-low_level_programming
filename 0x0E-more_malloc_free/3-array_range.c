86cdbd1e251a25fdebdb0d6d170ffb0c44a2ce67
Isaac <zuderg@gmail.com>
2021-07-23 21:16:45 -0700

    right length of array

--- 0x0E-more_malloc_free/3-array_range.c
+++ 0x0E-more_malloc_free/3-array_range.c
@@ -8,18 +8,18 @@
  */
 int *array_range(int min, int max)
 {
-int *p;
+int *p; 
 int i;
 if (min > max)
 {
 return (NULL);
-}
+} 
 p = malloc(sizeof(int) * (max - min + 1));
 if (p == NULL)
 {
 return (NULL);
 }
-for (i = 0; i < max - min; i++)
+for (i = 0; i <= max - min; i++)
 {
 p[i] = i + min;
 }
