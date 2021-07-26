a4a30155504edbd86d43ce16fb0f502d37fa264b
Isaac <zuderg@gmail.com>
2021-07-26 13:00:05 -0700

    fringe cases

--- 0x0F-structures_typedef/2-print_dog.c
+++ 0x0F-structures_typedef/2-print_dog.c
@@ -9,6 +9,8 @@
  */
 void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
 if (d->name == NULL)
 {
 d->name = "(nil)";
@@ -22,4 +24,5 @@ if (d->owner == NULL)
 d->owner = "(nil)";
 }
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
 }
