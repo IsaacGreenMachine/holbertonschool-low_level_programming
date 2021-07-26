b2e74f1472647b353d4c8e9c059213f9a2c00462
Isaac <zuderg@gmail.com>
2021-07-26 12:37:17 -0700

    fringe cases

--- 0x0F-structures_typedef/1-init_dog.c
+++ 0x0F-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initializes passed values for dog
  * @d: struct
@@ -10,7 +11,19 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d != NULL)
+{
+if (name != NULL)
+{
 d->name = name;
+}
+if (age >= 0)
+{
 d->age = age;
+}
+if (owner != NULL)
+{
 d->owner = owner;
+}
+}
 }
