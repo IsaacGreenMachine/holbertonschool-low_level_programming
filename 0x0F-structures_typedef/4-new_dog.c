795b4583d2ef945bdb52772be45c241ed627bd11
Isaac <zuderg@gmail.com>
2021-07-26 15:08:59 -0700

    reworked

--- 0x0F-structures_typedef/4-new_dog.c
+++ 0x0F-structures_typedef/4-new_dog.c
@@ -1,20 +1,6 @@
 #include "dog.h"
-#include <stdio.h>
-/**
- * init_dog - initializes passed values for dog
- * @d: struct
- * @name: string for name
- * @age: float for age
- * @owner: string for owner
- *
- * Return: void
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a dog with given parameters and passes pointer to dog
@@ -26,16 +12,28 @@ d->owner = owner;
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t my_dog;
-dog_t *ptr = &my_dog;
-if (ptr == NULL || name == NULL || age < 0 || owner == NULL)
+dog_t *dawg;
+dawg = malloc(sizeof(dog_t));
+if (dawg == NULL)
 {
+free(dawg);
 return (NULL);
 }
-init_dog(ptr, name, age, owner);
-if (ptr == NULL || ptr->name == NULL || ptr->age < 0 || ptr->owner == NULL)
+dawg->name = strdup(name);
+if (dawg->name == NULL)
 {
+free(dawg);
+free(dawg->name);
 return (NULL);
 }
-return (ptr);
+dawg->owner = strdup(owner);
+if (dawg->owner == NULL)
+{
+free(dawg);
+free(dawg->name);
+free(dawg->owner);
+return (NULL);
+}
+dawg->age = age;
+return (dawg);
 }
