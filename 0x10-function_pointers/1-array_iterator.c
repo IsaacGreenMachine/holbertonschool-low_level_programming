6e536302e70790f37ad90243dbf02f69b55fee05
Isaac <zuderg@gmail.com>
2021-07-27 10:14:27 -0700

    fringe cases

--- 0x10-function_pointers/1-array_iterator.c
+++ 0x10-function_pointers/1-array_iterator.c
@@ -9,9 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+if (array != 0 && action != 0)
+{
 unsigned int i = 0;
 for (i = 0; i < size; i++)
 {
 action(array[i]);
+}
 }
 }
