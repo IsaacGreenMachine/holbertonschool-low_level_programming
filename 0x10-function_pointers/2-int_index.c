c38eb6ede8e060f701610300bf21554c9f4b2786
Isaac <zuderg@gmail.com>
2021-07-27 11:46:15 -0700

    added script and updated header file

---
+++ 0x10-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+/**
+ * int_index - finds a number in an array based on a function
+ * @array: pointer to an array of ints
+ * @size: size of passed array
+ * @cmp: pointer to a function to be used
+ *
+ * Return: index of found number
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (cmp == NULL)
+{
+return (-1);
+}
+else
+{
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
+}
+}
+return (-1);
+}
+}
