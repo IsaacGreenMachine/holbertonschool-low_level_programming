5bd6edd5b535f3229c1a77d19c927650cfb07e1d
Isaac <zuderg@gmail.com>
2021-07-27 10:06:46 -0700

    added function and updated header file

---
+++ 0x10-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+/**
+ * array_iterator - uses a passed function on a passed array
+ * @array: passed array
+ * @size: size of passed array
+ * @action: function to be used on array
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i = 0;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
