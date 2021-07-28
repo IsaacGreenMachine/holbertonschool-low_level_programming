788af09803e9d72c61076fc591dbed499206ce44
Isaac <zuderg@gmail.com>
2021-07-27 17:14:45 -0700

    fringe cases

--- 0x10-function_pointers/3-main.c
+++ 0x10-function_pointers/3-main.c
@@ -19,7 +19,11 @@ int (*p)(int, int);
 /*making sure args right length*/
 if (argc == 4 && strlen(argv[2]) == 1)
 {
-
+if (strstr("+-/*%", argv[2]) == NULL)
+{
+printf("Error\n");
+return (0);
+}
 /*setting pointer equal to right function*/
 p = get_op_func((argv[2]));
 
