09a5598bf22647e8df1b0c0569ddc0423be2cfbc
Isaac <zuderg@gmail.com>
2021-07-29 15:56:59 -0700

    added header file and scripts

---
+++ 0x11-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sums all passed arguments
+ * @n: number of arguments
+ * Return: sum of passed arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list ap;
+unsigned int count;
+int sum = 0;
+if (n == 0)
+{
+return (0);
+}
+va_start(ap, n);
+for (count = 0; count < n; count++)
+{
+sum = sum + va_arg(ap, int);
+}
+va_end(ap);
+return (sum);
+}
