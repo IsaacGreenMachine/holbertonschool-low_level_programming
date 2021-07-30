9b287e7b9f47506817a6fa3ae54ce84c9d760e04
Isaac <zuderg@gmail.com>
2021-07-30 09:55:19 -0700

    betty

--- 0x11-variadic_functions/3-print_all.c
+++ 0x11-variadic_functions/3-print_all.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * print_all - prints all passed args
+ * @format: format of args to be passed
+ *
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
 const char *c = format;
@@ -8,32 +14,32 @@ int pointless = 0;
 char *del;
 char *s;
 va_start(ap, format);
-while(*c != '\0')
+while (*c != '\0')
 {
 del = ", ";
-if(*(c + 1) == '\0')
+if (*(c + 1) == '\0')
 del = "";
-switch (*c){
-	case 'c':
-		printf("%c%s", va_arg(ap, int), del);
-		break;
-	case 'i':
-                printf("%i%s", va_arg(ap, int), del);
-                break;
-	case 'f':
-                printf("%f%s", va_arg(ap, double), del);
-                break;
-	case 's':
-		s = va_arg(ap, char *);
-		if (s == NULL)
-		printf("%s%s", "(nil)", del);
-		printf("%s%s", s, del);
-                
-		break;
-	default:
-		pointless += 1;	
+switch (*c)
+{
+case 'c':
+printf("%c%s", va_arg(ap, int), del);
+break;
+case 'i':
+printf("%i%s", va_arg(ap, int), del);
+break;
+case 'f':
+printf("%f%s", va_arg(ap, double), del);
+break;
+case 's':
+s = va_arg(ap, char *);
+if (s == NULL)
+printf("%s%s", "(nil)", del);
+printf("%s%s", s, del);
+break;
+default:
+pointless += 1;
 }
-		c++;
+c++;
 }
 printf("\n");
 va_end(ap);
