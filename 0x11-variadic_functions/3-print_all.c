5579a89a5d19eeda7276eb79f10eac80b6711f78
Isaac <zuderg@gmail.com>
2021-07-30 09:45:44 -0700

    redid script and updated header file

--- 0x11-variadic_functions/3-print_all.c
+++ 0x11-variadic_functions/3-print_all.c
@@ -1,91 +1,40 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
-/**
- * print_all - prints all passed arguments
- * @format: string format of arguments
- *
- * Return: Always void
- */
 void print_all(const char * const format, ...)
 {
-va_list ap;
 const char *c = format;
-void (*f)(va_list);
-va_start(ap, format);
-while (*c != '\0')
-{
-f = getOps(*c);
-f(ap);
-c++;
-}
-va_end(ap);
-}
-/**
- * getOps - returns function pointer based on char
- * @s: passed char
- *
- * Return: function pointer
- */
-void (*getOps(char s))(va_list)
-{
-int i = 0;
-op_t ops[] = {
-{'c', print_c},
-{'i', print_i},
-{'f', print_f},
-{'s', print_s},
-{0, NULL}
-};
-while (ops[i].c != s && i < 5)
-{
-i++;
-}
-return (ops[i].f);
-}
-/**
- * print_c - prints a char
- * @arg: valist
- *
- * Return: void
- */
-void print_c(va_list arg)
-{
-printf("%c", va_arg(arg, int));
-}
-/**
- * print_i - prints int
- * @arg: valist
- *
- * Return: void
- */
-void print_i(va_list arg)
-{
-printf("%i", va_arg(arg, int));
-}
-/**
- * print_f - prints a float
- * @arg: valist
- *
- * Return: void
- */
-void print_f(va_list arg)
-{
-printf("%f", va_arg(arg, double));
-}
-/**
- * print_s - prints a string
- * @arg: valist
- *
- * Return: void
- */
-void print_s(va_list arg)
-{
+va_list ap;
+int pointless = 0;
+char *del;
 char *s;
-s = va_arg(arg, char*);
-if (s == NULL)
+va_start(ap, format);
+while(*c != '\0')
 {
-printf("(nil)");
+del = ", ";
+if(*(c + 1) == '\0')
+del = "";
+switch (*c){
+	case 'c':
+		printf("%c%s", va_arg(ap, int), del);
+		break;
+	case 'i':
+                printf("%i%s", va_arg(ap, int), del);
+                break;
+	case 'f':
+                printf("%f%s", va_arg(ap, double), del);
+                break;
+	case 's':
+		s = va_arg(ap, char *);
+		if (s == NULL)
+		printf("%s%s", "(nil)", del);
+		printf("%s%s", s, del);
+                
+		break;
+	default:
+		pointless += 1;	
 }
-printf("%s", s);
+		c++;
+}
+printf("\n");
+va_end(ap);
 }
--- 0x11-variadic_functions/variadic_functions.h
+++ 0x11-variadic_functions/variadic_functions.h
@@ -1,23 +1,7 @@
 #ifndef VARIADICFUNCTIONS_H
 #define VARIADICFUNCTIONS_H
-#include <stdarg.h>
-/**
- * struct ops - Struct op
- * @c: The char
- * @f: The function associated
- */
-typedef struct ops
-{
-char c;
-void (*f)(va_list ap);
-} op_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void (*getOps(char s))(va_list);
-void print_c(va_list arg);
-void print_i(va_list arg);
-void print_f(va_list arg);
-void print_s(va_list arg);
 #endif
