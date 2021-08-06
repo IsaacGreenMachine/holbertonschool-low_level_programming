b563d66972a6a3b3f9771406141d4c382555cde0
IsaacGreenMachine <zuderg@gmail.c>
2021-08-06 13:49:13 -0700

    fixed string NULL fringe case

--- 0x13-singly_linked_lists/2-add_node.c
+++ 0x13-singly_linked_lists/2-add_node.c
@@ -10,13 +10,21 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
 new = malloc(sizeof(list_t));
-if (new == NULL || head == NULL)
-return (NULL);
-new->str = strdup(str);
-if (new->str == NULL)
-return (NULL);
+	if (new == NULL || head == NULL)
+	return (NULL);
+	if (str == NULL)
+	{
+		new->str = NULL;
+		new->len = 0;
+	}
+	else
+	{
+		new->str = strdup(str);
+		new->len = (strlen(str));
+	}
+	if (new->str == NULL)
+	return (NULL);
 new->next = *head;
-new->len = (strlen(str));
 *head = new;
 return (new);
 
