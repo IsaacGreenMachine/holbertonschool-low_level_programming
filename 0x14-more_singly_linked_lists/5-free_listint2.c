d7b37c076c75471f2b13dd8d75cd210792b11635
IsaacGreenMachine <zuderg@gmail.c>
2021-08-10 13:25:06 -0700

    added scripts, header file, and README

---
+++ 0x14-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint
+ * @head: head node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *next, *hedd;
+hedd = *head;
+for (next = hedd; next != NULL; next = hedd->next, free(hedd), hedd = next)
+{
+}
+head = NULL;
+}
