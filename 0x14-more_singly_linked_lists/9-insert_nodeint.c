44a9da2fafc19eee3a45a6a7b931ff217b892045
IsaacGreenMachine <zuderg@gmail.c>
2021-08-11 13:48:34 -0700

    added add node function

--- 0x14-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x14-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,3 +39,27 @@ ptr->next = new;
 
 return (new);
 }
+
+/**
+ * add_nodeint - adds a node to the beginning of a listint
+ * @head: start of list int
+ * @n: value to put in new node
+ * Return: pointer to new head
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+if (*head == NULL)
+{
+new->next = NULL;
+}
+else
+{
+new->next = *head;
+}
+new->n = n;
+*head = new;
+return (new);
+}
