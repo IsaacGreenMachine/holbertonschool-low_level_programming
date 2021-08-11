77a2f5da42ada7e2e4a0d6e06fea3363a782923a
IsaacGreenMachine <zuderg@gmail.c>
2021-08-11 10:21:28 -0700

    fringe case HEAD == NULL

--- 0x14-more_singly_linked_lists/5-free_listint2.c
+++ 0x14-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@
 void free_listint2(listint_t **head)
 {
 listint_t *next, *hedd;
-hedd = *head;
-if (head != NULL)
+if (head != NULL && *head != NULL)
 {
+hedd = *head;
 for (next = hedd; next != NULL; next = hedd->next, free(hedd), hedd = next)
 {
 }
-}
 *head = NULL;
+}
 }
