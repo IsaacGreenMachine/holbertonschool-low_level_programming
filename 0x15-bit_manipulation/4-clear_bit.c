ed5b033985eb000a4d2ee0eb83c1fb88b834b838
IsaacGreenMachine <zuderg@gmail.c>
2021-08-12 14:32:28 -0700

    fixed final 0 error

--- 0x15-bit_manipulation/4-clear_bit.c
+++ 0x15-bit_manipulation/4-clear_bit.c
@@ -10,12 +10,13 @@ int clear_bit(unsigned long int *n, unsigned int index)
 unsigned int i = 0, count = 64;
 if (index > 64)
 return (-1);
-for (count = 63; count > 0; count--)
+for (count = 64; count > 0; count--)
 {
 if (count != index)
 i++;
 i <<= 1;
 }
+i++;
 *n = (*n & i);
 return (1);
 }
