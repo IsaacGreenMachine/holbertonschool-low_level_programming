dfe7b72f731e71ed633e7c75ef94c52beeea458c
IsaacGreenMachine <zuderg@gmail.c>
2021-08-12 14:22:10 -0700

    added script and updated header file

---
+++ 0x15-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * clear_bit - clears a bit at a given point
+ * @n: pointer to number
+ * @index: index to be changed
+ * Return: 1 if successful, -1 if failed
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned int i = 0, count = 64;
+if (index > 64)
+return (-1);
+for (count = 64; count > 0; count--)
+{
+if (count != index)
+i++;
+i <<= 1;
+}
+*n = (*n & i);
+return (1);
+}
