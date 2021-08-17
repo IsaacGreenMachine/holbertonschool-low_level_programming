6b63f08eec2daf272ed4a4d594b97428a6bbda66
IsaacGreenMachine <zuderg@gmail.c>
2021-08-17 11:44:35 -0700

    appeased betty

--- 0x16-file_io/0-read_textfile.c
+++ 0x16-file_io/0-read_textfile.c
@@ -1,9 +1,18 @@
 #include "holberton.h"
+/**
+ * read_textfile - reads a text file and prints it to standard output.
+ * @filename: name of file to be read
+ * @letters: number of letters to be read
+ *
+ * Return: 0 if fails, printed chars if success
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int i, j, k;
-char *buf; 
+char *buf;
 if (filename == NULL)
+return (0);
+if (letters == 0)
 return (0);
 buf = malloc(letters);
 i = open(filename, O_RDONLY);
