4a7f7cf147568a114e59626e0a8e360366dd4934
IsaacGreenMachine <zuderg@gmail.c>
2021-08-17 11:46:13 -0700

    fringe case with malloc

--- 0x16-file_io/0-read_textfile.c
+++ 0x16-file_io/0-read_textfile.c
@@ -10,17 +10,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int i, j, k;
 char *buf;
+
 if (filename == NULL)
 return (0);
+
 if (letters == 0)
 return (0);
+
 buf = malloc(letters);
+if (buf == NULL)
+return (0);
+
 i = open(filename, O_RDONLY);
 if (i == -1)
 return (0);
+
 j = read(i, buf, letters);
 if (j == -1)
 return (0);
+
 k = write(1, buf, j);
 if (k == -1)
 return (0);
