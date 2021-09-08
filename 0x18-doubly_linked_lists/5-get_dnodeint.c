b0656ce5cf82533b65cbfd3c91e0c75ce0c1af13
IsaacGreenMachine <zuderg@gmail.c>
2021-09-08 11:40:41 -0700

    fixed header file

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 for (; index > 0; index--)
 {
 head = head->next;
-if (head == NULL)
+if (head->next == NULL)
 return (NULL);
 }
 return (head);
--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -9,14 +11,11 @@
  * Description: doubly linked list node structure
  * for Holberton project
  */
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
