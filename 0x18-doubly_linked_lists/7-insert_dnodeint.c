bcb20f85a8c44c39679ea71626905c0df12c18a6
IsaacGreenMachine <zuderg@gmail.c>
2021-09-09 08:10:21 -0700

    added fringe cases

--- 0x18-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x18-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,43 +6,17 @@
  * @n: value to put in node
  * Return: address of node or NULL if failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *a, *b = NULL, *new = malloc(sizeof(dlistint_t));
 if (new == NULL || h == NULL)
 return (NULL);
+if (idx == 0)
+return (add_dnodeint(h, n));
 for (a = *h; a->next != NULL; a = a->next, idx--)
 {
 if (idx == 0)
-return (assignNode(a, b, new, n));
-}
-if (idx == 0)
-{
-return (assignNode(a, b, new, n));
-}
-else if (idx == 1)
-{
-a->next = new;
-new->next = NULL;
-new->prev = a;
-return (new);
-}
-else
-{
-free(new);
-return (NULL);
-}
-}
-
-/**
- * assignNode - assigns a node with given data
- * @a: next node
- * @b: prev node
- * @new: new node
- * @n: value inserted into node
- * Return: address of new node
- */
-dlistint_t *assignNode(dlistint_t *a, dlistint_t *b, dlistint_t *new, int n)
 {
 b = a->prev;
 b->next = new;
@@ -51,4 +25,25 @@ new->prev = b;
 new->next = a;
 new->n = n;
 return (new);
+}
+}
+if (idx == 0)
+{
+b = a->prev;
+b->next = new;
+a->prev = new;
+new->prev = b;
+new->next = a;
+new->n = n;
+return (new);
+}
+else if (idx == 1)
+{
+return (add_dnodeint_end(h, n));
+}
+else
+{
+free(new);
+return (NULL);
+}
 }
