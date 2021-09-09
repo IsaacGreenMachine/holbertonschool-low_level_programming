374c956dffa898b24848e27c69cd8cfa90dce80a
IsaacGreenMachine <zuderg@gmail.c>
2021-09-09 08:34:53 -0700

    fringe cases and header file

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,10 +8,24 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *a, *b, *c;
-for (a = *head; index > 0; a = a->next, index--)
+if (head == NULL || *head == NULL)
+return (-1);
+if (index == 0)
 {
-}
+a = *head;
 b = a->next;
+if (b != NULL)
+b->prev = NULL;
+*head = b;
+free(a);
+return (1);
+}
+for (b = *head; index > 0; b = b->next, index--)
+{
+if (b == NULL)
+return (-1);
+}
+a = b->prev;
 c = b->next;
 a->next = c;
 c->prev = a;
