3d94fb4ef4215363efeb477342c1726cd2c17d9d
Isaac Green <zuderg@gmail.com>
2022-02-16 10:41:24 -0600

    moving to vagrant

---
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+hash_table_t *hash_table_create(unsigned long int size){
+struct hash_table_t *array = NULL;
+
+/*for (int i = 0; i<size; i++) {
+array[i] =
+}
+
+hash_table_t test = {size, };
+
+return &test;
+
+
+// create an array of size size, for length of size, malloc pointer in each slot. return pointer to array
+
+*/
+}
