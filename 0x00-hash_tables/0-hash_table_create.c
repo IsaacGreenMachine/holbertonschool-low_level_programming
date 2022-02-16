56a91e7e06ae9dc697b91c39de0f9ef60e23e177
Isaac <zuderg@gmail.com>
2022-02-16 17:14:13 +0000

    added task 0

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -1,17 +1,22 @@
 #include "hash_tables.h"
-hash_table_t *hash_table_create(unsigned long int size){
-struct hash_table_t *array = NULL;
-
-/*for (int i = 0; i<size; i++) {
-array[i] =
+/**
+ * hash_table_create - creates and returns a hash table.
+ * @size: size of hash table to be created
+ * Return: pointer to a hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *hashTable = malloc(sizeof(hash_table_t));
+if (hashTable == NULL)
+return (NULL);
+hashTable->size = size;
+hashTable->array = malloc(sizeof(hash_node_t *) * size);
+if (hashTable->array == NULL)
+{
+free(hashTable);
+return (NULL);
 }
-
-hash_table_t test = {size, };
-
-return &test;
-
-
-// create an array of size size, for length of size, malloc pointer in each slot. return pointer to array
-
-*/
+for (unsigned int i = 0; i < size; i++)
+hashTable->array[i] = NULL;
+return (hashTable);
 }
