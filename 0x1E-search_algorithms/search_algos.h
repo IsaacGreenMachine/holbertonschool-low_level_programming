514c3c7ff233204e3039f91a902ea01bd6eb332a
Isaac <zuderg@gmail.com>
2022-03-24 15:46:49 +0000

    added header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t size);
 int advanced_binary(int *array, size_t size, int value);
+int adv_bin_search(int *array, int start, size_t size, int value);
+int find_lowest(int *array, int pos);
 #endif
