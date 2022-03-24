d54e4b955befae99846496a2e4eabf967b3526c3
Isaac <zuderg@gmail.com>
2022-03-24 15:46:06 +0000

    made recursive

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -9,12 +9,25 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-int L = 0;
-int R = size - 1;
-int m = 0;
+return (adv_bin_search(array, 0, size - 1, value));
+}
+
+/**
+ * adv_bin_search - implements a binary search that finds first occurance
+ * @array: whole array
+ * @start: starting pos
+ * @end: ending pos
+ * @value: value to search array for
+ * Return: index of @value or -1 if not present
+ */
+int adv_bin_search(int *array, int start, size_t end, int value)
+{
+int L = start;
+int R = end;
+int m = start;
 if (array == NULL)
 return (-1);
-for (; L <= R; )
+if (L <= R)
 {
 printf("Searching in array: ");
 print_array(&array[L], R - L + 1);
@@ -24,12 +37,10 @@ L = m + 1;
 else if (array[m] > value)
 R = m - 1;
 else
-{
-while (array[m - 1] == array[m])
-m--;
-return (m);
+return (find_lowest(array, m));
+return (adv_bin_search(array, L, R, value));
 }
-}
+else
 return (-1);
 }
 
@@ -52,4 +63,19 @@ printf("%d", array[i]);
 ++i;
 }
 printf("\n");
+}
+
+/**
+ * find_lowest - returns lowest index of repeating vals in an array
+ * @array: whole array
+ * @pos: post of val
+ * Return: index of @value or -1 if not present
+ */
+int find_lowest(int *array, int pos)
+{
+if (pos == 0)
+return (pos);
+if (array[pos] == array[pos - 1])
+return (find_lowest(array, pos - 1));
+return (pos);
 }
