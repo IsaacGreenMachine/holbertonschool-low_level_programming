31d7040c7f7ad42127871e99c342aebe12518c9e
Isaac <zuderg@gmail.com>
2022-03-24 14:56:59 +0000

    added task and updated header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,4 +7,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t size);
+int advanced_binary(int *array, size_t size, int value);
 #endif
