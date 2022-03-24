7781914aa4204556547873cab71885688f9647ed
Isaac <zuderg@gmail.com>
2022-03-24 16:32:22 +0000

    R = m, != m - 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t size);
 int advanced_binary(int *array, size_t size, int value);
-int adv_bin_search(int *array, int start, size_t size, int value);
+int adv_bin_search(int *array, int start, size_t end, int value);
 int find_lowest(int *array, int pos);
+int find_highest(int *array, int pos, int size);
 #endif
